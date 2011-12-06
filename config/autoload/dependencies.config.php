d49b0fe344a92dac343c899176bad5fb3ea09b97
Nigel Lundsten <nigel.lundsten@gmail.com>
2011-12-06 12:45:44 -0700

    Add various config files for added submodules

---
+++ config/autoload/dependencies.config.php
@@ -0,0 +1,9 @@
+<?php
+return array('di' => array(
+    'instance' => array(
+        'alias' => array(
+            // Which user service for SpeckCatalog to use
+            'speckcatalog_user_service' => 'edpuser_user_service',
+        ),
+    ),
+));
