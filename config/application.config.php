d2c953c65af491ba4335f7b936bb5fbf980481ef
Evan Coury <me@evancoury.com>
2011-12-09 13:55:47 -0700

    Update zf2 submodule and adapt for latest changes

--- config/application.config.php
+++ config/application.config.php
@@ -1,9 +1,5 @@
 <?php
 return array(
-    'module_paths' => array(
-        realpath(dirname(__DIR__) . '/module'),
-        realpath(dirname(__DIR__) . '/vendor'),
-    ),
     'modules' => array(
         'Application',
         'SpiffyDoctrine',
@@ -13,7 +9,10 @@ return array(
     ),
     'module_listener_options' => array( 
         'config_cache_enabled'    => false,
-        'cache_dir'               => realpath(dirname(__DIR__) . '/data/cache'),
-        'application_environment' => getenv('APPLICATION_ENV'),
+        'cache_dir'               => './data/cache',
+        'module_paths' => array(
+            './module',
+            './vendor',
+        ),
     ),
 );
