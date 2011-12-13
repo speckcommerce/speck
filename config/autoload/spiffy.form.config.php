3826fbb05e695874ae204b36061e4deb37b7e030
Nigel Lundsten <nigel.lundsten@gmail.com>
2011-12-13 07:14:39 -0700

    Add SpiffyForm config

---
+++ config/autoload/spiffy.form.config.php
@@ -0,0 +1,8 @@
+<?php
+return array('di' => array(
+    'instance' => array(
+        'alias' => array(
+            'spiffy_annotation_cache' => 'Doctrine\Common\Cache\ApcCache'
+        ),
+    )  
+));
