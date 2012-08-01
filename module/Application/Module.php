0b25ff5b56251b6c746a43c57fe3857e0dc76473
Evan Coury <me@evancoury.com>
2012-07-31 23:44:58 -0700

    Revert "Update SpeckCatalog"
    This reverts commit bb47527f65b731c2dd57657039c3c362ec98533e.
    
    Accidental commit -a with some debug stuff.

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -9,14 +9,6 @@ class Module
         return include __DIR__ . '/config/module.config.php';
     }
 
-    public function onBootstrap($e)
-    {
-        $sem = $e->getApplication()->getEventManager()->getSharedManager();
-        $sem->attach('ZfcUser\Form\Register', 'init', function($e) {
-            die('register form!');
-        });
-    }
-
     public function getAutoloaderConfig()
     {
         return array(
