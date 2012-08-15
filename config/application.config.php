7f9f57c12452465a2ce3609babaf6b7ece36e262
Ben Youngblood <bx.youngblood@gmail.com>
2012-08-14 21:40:06 -0700

    enable speckuserdashboard and add css

--- config/application.config.php
+++ config/application.config.php
@@ -8,6 +8,7 @@ return array(
         'SpeckCatalog',
         'SpeckAddress',
         'SpeckUserAddress',
+        'SpeckUserDashboard',
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
