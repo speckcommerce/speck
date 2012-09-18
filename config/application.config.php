2e3f591dbd317be1f4947672ef2ab58a859777fd
nigel lundsten <nigel. lundsten@gmail.com>
2012-09-18 15:59:45 -0700

    add multisite to config, update speckcatalog to latest master, update multisite to include new commits (exception catching)

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,7 @@ return array(
         'Application',
         'ZfcUser',
         'ZfcBase',
-        'SpeckMultiSite',
+        'SpeckMultisite',
         'SpeckCart',
         'SpeckCatalog',
         'SpeckAddress',
