5538a304984de948bdeea3c1d6fe29e6d43813c0
nigel lundsten <nigel. lundsten@gmail.com>
2013-02-04 10:49:29 -0700

    add AtPhpSettings

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,7 +1,13 @@
 <?php
 return array(
-
     'zfcadmin' => array(
         'admin_layout_template' => 'layout/speckadmin',
+    ),
+    'php_settings'   => array(
+        'display_startup_errors'     => false,
+        'display_errors'             => false,
+        'max_execution_time'         => 30,
+        'date.timezone'              => 'UTC',
+        'mbstring.internal_encoding' => 'UTF-8',
     ),
 );
